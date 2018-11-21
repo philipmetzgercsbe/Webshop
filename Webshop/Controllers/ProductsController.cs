38b02d8c681cbc59bc566d45d942f1f55e0ef295
samuel.gast <hoechstesetablissement@gmail.com>
2018-11-21 16:12:17 +0100

    Latest commit

--- Webshop/Controllers/ProductsController.cs
+++ Webshop/Controllers/ProductsController.cs
@@ -15,12 +15,8 @@ namespace Webshop.Controllers
 
         public ProductsController(WebshopContext context)
         {
-            var Products = _context.Products
-                .Include(a => a.Name)
-                .Include(a => a.Imagepath)
-                .Include(a => a.Description)
-                .Include(a => a.Price)
-                .Include(a => a.Stock);
+            var Products = _context.Products.ToList();
+                
             ProductViewModel viewModel = new ProductViewModel();
             viewModel.Products = Products.ToList();
             _context = context;
