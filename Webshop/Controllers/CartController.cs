8ea8f6b8ab19795aba2f7be7d948710855d04244
philipmetzgercsbe <philip.metzger@csbe.ch>
2018-11-23 13:29:06 +0100

    finished ChangeAmount Method in Cartcontroller deleted the Dictionary in Cart.cshtml

--- Webshop/Controllers/CartController.cs
+++ Webshop/Controllers/CartController.cs
@@ -16,59 +16,85 @@ namespace Webshop.Controllers
     {
 
         private readonly WebshopContext _context;
-       
+
 
         public CartController(WebshopContext context)
         {
             _context = context;
         }
-       
+
         public IActionResult Cart()
         {
             CartViewModel viewModel = new CartViewModel();
 
-           
+
             var carts = _context.Carts.Include(a => a.Products).Where(a => a.User.UserID == viewModel.userId).ToList();
-            var prodamounts = _context.Carts.Include(a => a.ProductAmount).Where(a => a.User.UserID == viewModel.userId).ToList();
+            var prodamounts = _context.Carts.Include(a => a.ProductAmount).Include(a => a.Products)
+                .Where(a => a.User.UserID == viewModel.userId).ToList();
             viewModel.Products = carts[0].Products.ToList();
             foreach (var product in viewModel.Products)
             {
+
                 viewModel.ProductAmount = prodamounts[0].ProductAmount;
             }
+
             if (viewModel.Products == null)
             {
                 ViewData["Message"] = "No items in your cart";
             }
+
             return View(viewModel);
         }
 
-//        [HttpPost]
-//        public IActionResult ChangeAmount(int id,int newAmount)
-//        {
-//           CartViewModel viewModel = new CartViewModel();
-//            
-//            var cart =  _context.Carts
-//                .FirstOrDefault(m => m.CartID == id);
-//            var product = _context.Products
-//                .FirstOrDefault(m => m.ProductID == cart.ProductFK);
-//
-//            if (cart != null)
-//            {
-//                var productAmount = cart.ProductAmount;
-//                var productStock = product.Stock;
-//                if ( productAmount <= productStock && productAmount > 0)
-//                {               
-//                        viewModel.ProductAmount++;
-//                }
-//                if (productAmount == productStock)
-//                {
-//                    return LocalRedirect("Cart");
-//                }
-//
-//               
-//            }
-//            return viewModel.ProductAmount++;
-//        }
+        [HttpPost]
+        public IActionResult ChangeAmount(int id, int newAmount, bool add)
+        {
+            CartViewModel viewModel = new CartViewModel();
+
+            var cart = _context.Carts
+                .FirstOrDefault(m => m.CartID == id);
+            var cartItems = cart.Products.ToList();
+            var product = _context.Products
+                .FirstOrDefault(m => m.ProductID == cartItems.Find(a => a.ProductID == id).ProductID);
+
+            if (add == true && cart != null)
+            {
+                var productAmount = cart.ProductAmount;
+                var productStock = product.Stock;
+                if (productAmount <= productStock && productAmount > 0)
+                {
+                    viewModel.ProductAmount++;
+                }
+
+                if (productAmount == productStock)
+                {
+                    return LocalRedirect("Cart");
+                }
+
+                return View("Cart");
+
+
+            }
+            else
+            {
+                var productAmount = cart.ProductAmount;
+                var productStock = product.Stock;
+                if (productAmount <= productStock && productAmount > 0)
+                {
+                    viewModel.ProductAmount--;
+                }
+
+                if (productAmount == 1)
+                {
+                    return LocalRedirect("Cart");
+                }
+
+                return View("Cart");
+            }
+
+        }
+    
+
 //        //Do the same for DecreaseAmount
 //        public IActionResult AddProduct(int id)
 //        {
